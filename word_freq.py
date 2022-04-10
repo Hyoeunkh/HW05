a878a7ec0e70e6c523f5f1b7a11a3566812ff1a5
Hyoeunkh <lhe4528@knu.ac.kr>
2022-04-10 05:58:23 -0700

    word_freq.py

---
+++ word_freq.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python
+import sys
+import re
+text = str(sys.argv[1])
+num = int(sys.argv[2])
+
+file = open(text, 'r')
+lines = file.readlines()	
+for i in lines:
+	i = re.sub("[,.!?]", "", i)
+	word = i.split()
+	dic = {}
+	for w in word:
+		if w not in dic: 
+			dic[w] = dic.get(w,0) + 1
+		else:	
+			dic[w] += 1
+	
+	sort = sorted(dic.items(), key=lambda x: x[1], reverse=True)	
+for x in range(num):
+	print("%-15s%5d" % (sort[x][0], sort[x][1]))
+
+file.close() 
